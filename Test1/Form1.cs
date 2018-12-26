b405eaf44c00d54449056101200d3df8f44a049b
mendozagit <mendoza.git@gmail.com>
2018-12-25 22:11:03 -0600

    Version 1

--- Test1/Form1.cs
+++ Test1/Form1.cs
@@ -16,5 +16,10 @@ namespace Test1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola");
+        }
     }
 }
